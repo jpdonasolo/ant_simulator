9987c4fdaba8cb8ae78623ef38421072bbccf1f1
jpdonasolo <jpdonasolo@gmail.com>
2022-03-29 23:48:23 -0300

    Rename variables

--- headers/world.h
+++ headers/world.h
@@ -25,58 +25,58 @@ public:
     Fora da classe World, trabalhamos com duas coordenadas, x e 
     y, por facilidade. Internamente, a classe converte essas duas
     coordenadas para o índice representativo daquelas coordenadas
-    no vetor World::chart, que guarda todas as entidades presentes
+    no vetor World::m_chart, que guarda todas as entidades presentes
     no mapa
     */
     int posToInt(int posx, int posy){
-        return posx + width * posy;
+        return posx + m_width * posy;
     }
  
     void addAntToChart(Ant * ant, int posx, int posy);
 
 private:
     // Mapa
-    std::vector<Entity *> chart;
+    std::vector<Entity *> m_chart;
     /*
     Vetor que rastreia todas as formigas do mundo. Será usado
     para paralelismo. Ponteiro de ponteiro pois vai apontar
     para a posição da formiga no mapa.
     */
-    std::vector<Ant **> ants;
-    const int width, height;
+    std::vector<Ant **> m_ants;
+    const int m_width, m_height;
 };
 
 
 void World::print()
 {
-    for (int pos = 0; pos < chart.size(); pos++){
+    for (int pos = 0; pos < m_chart.size(); pos++){
         
-        cout << chart[pos]->getMarker();
+        cout << m_chart[pos]->getMarker();
         
-        if ((pos + 1 ) % width == 0){
+        if ((pos + 1 ) % m_width == 0){
             cout << endl;
         };
     }
 }
 
 World::World(int width, int height)
-    : width(width), height(height)
+    : m_width(width), m_height(height)
 {
-    chart.resize(width * height);
-    Entity * emptySpace =  new Entity();
+    m_chart.resize(m_width * m_height);
+    Entity * emptySpace =  new EmptySpace();
 
     /*
     Todos os espaços vazios apontam para a mesma entidade 
     Entity, que representa um espaço vazio, para salvar
     memória.
     */
-    for (int i = 0; i < chart.size(); i++){
-        chart[i] = emptySpace;
+    for (int i = 0; i < m_chart.size(); i++){
+        m_chart[i] = emptySpace;
     }
 }
 
 void World::addAntToChart(Ant * ant, int posx, int posy)
 {    
     const int indexInChart = posToInt(posx, posy);
-    chart[indexInChart] = ant;
+    m_chart[indexInChart] = ant;
 }
